2f1e1a166924100fc9ff3efc035d35a22164fea9
lentrix <hawkmanlentrix@gmail.com>
2021-07-05 10:35:56 +0800

    start asset

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AssetController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -21,5 +22,11 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::group(['middleware'=>'auth:api'], function(){
     Route::get('/user', [AuthController::class, 'me']);
     Route::post('/logout',[AuthController::class, 'logout']);
+
+    Route::post('/assets/search', [AssetController::class, 'search']);
+    Route::post('/assets', [AssetController::class, 'store']);
+    Route::get('/assets/{asset}', [AssetController::class, 'show']);
+    Route::put('/assets/{asset}', [AssetController::class, 'update']);
+    Route::delete('/assets/{asset}', [AssetController::class, 'destroy']);
 });
 
